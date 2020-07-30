cfeefea5f4816c3955e78178f8b49aa17e40a118
Ed.Cherkashchyn <cherkashchyn04042019>
2020-07-30 12:23:49 +0300

    Create module Overdose_AdvancedReview

---
+++ app/code/Eduard_Cherkashyn/Overdose_AdvancedReview/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'EduardCherkashyn_OverdoseAdvancedReview',
+    __DIR__
+);
